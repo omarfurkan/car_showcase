5dccf06dcd239fc39bf449a8b029d0c7147ebcfa
omarfurkan <omarfurkan92@gmail.com>
2023-08-09 11:54:38 +0600

    image of car fetching from imagin studio

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { CarProps } from "@/types";
-import { calculateCarRent } from "@/utils";
+import { calculateCarRent, generateCarImageUrl } from "@/utils";
 import Image from "next/image";
 import { useState } from "react";
 import { CarDetails, CustomButton } from "./";
@@ -42,7 +42,7 @@ const CarCard = ({ car }: CarCardProps) => {
 
       <div className="relative w-full h-40 my-3 object-contain">
         <Image
-          src="/hero.png"
+          src={generateCarImageUrl(car)}
           alt="car model"
           fill
           priority
